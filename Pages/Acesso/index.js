dbfdb3a335d4e39d441473e081befbb7c5d86760
correaa1 <bruno_correa_ds@hotmail.com>
2021-01-28 21:45:13 -0300

    Acesso

---
+++ Pages/Acesso/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Acesso = () => {
+  return (
+    <Container>
+      <Text>Acesso</Text>
+    </Container>
+  );
+};
+
+export default Acesso;
