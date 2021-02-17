0b481b99c41b44cb8fe3dfa40d604960e5362546
correaa1 <bruno_correa_ds@hotmail.com>
2021-02-17 19:26:32 -0300

    atualizando

--- Pages/Acesso/Ficha/index.js
+++ Pages/Acesso/Ficha/index.js
@@ -3,8 +3,20 @@ import React from 'react';
 import {Text} from 'react-native';
 
 import {Container} from './styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Ficha = () => {
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('nome');
+      if (value) {
+        alert(value);
+        return value;
+      }
+    } catch (e) {
+      // error reading value
+    }
+  };
   return (
     <Container>
       <Text>Ficha</Text>
